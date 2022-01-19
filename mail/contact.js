71b1732816c971042040cf5146ef3b63be7683a3
Bilel Aoun <59199755+Spittzzer@users.noreply.github.com>
2022-01-19 06:47:29 +0100

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -64,10 +64,7 @@ $(function () {
     },
   });
 
-  // $('a[data-toggle="tab"]').click(function (e) {
-  //   e.preventDefault();
-  //   $(this).tab("show");
-  // });
+  
 });
 
 $("#name").focus(function () {
