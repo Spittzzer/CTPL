325b35cd33a9fdca880a12cd9d3d3b31ededdeef
Bilel Aoun <59199755+Spittzzer@users.noreply.github.com>
2022-01-19 06:48:07 +0100

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -42,62 +42,7 @@ if($mail->send()){
 $mail->smtpClose();
 
 
-////////////////////////////////works but not hosted/////////////////////////////////////////////////
 
-// if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-//   http_response_code(500);
-//   exit();
-//   }
- 
-//  $name = strip_tags(htmlspecialchars($_POST['name']));
-//    $email = strip_tags(htmlspecialchars($_POST['email']));
-//   $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-//   $message = strip_tags(htmlspecialchars($_POST['message']));
- 
-//   $to = "billaoun@gmail.com";
-//   $subject = "$m_subject: $name";
-//  $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName:
-//  $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-//  $header = "From: $email";
-//  $header .= "Reply-To: $email";
- 
- 
- 
-//  if(!mail($to, $subject, $body, $header)){
-//  http_response_code(500);
- 
-//  }
-//  header('Access-Control-Allow-Origin: *');
- 
-//  header('Access-Control-Allow-Methods: GET, POST');
- 
-//  header("Access-Control-Allow-Headers: X-Requested-With");
-
-
-///////////////////not working/////////////////////////////////////////////////////////////
-
-
-
-// if(isset($_POST["name"]) && !empty($_POST["name"])&&isset($_POST["subject"])&&
-//!empty($_POST["subject"])&&isset($_POST["message"])&&!empty($_POST["message"])){
-// $to = 'billaoun@gmail.com';
-// $first= $_POST['firstname'];
-// $last = $_POST['lastname'];
-// $email = $_POST['email'];
-// $message = $_POST['message'];
-
-
-// $data = array(
-// 'first' => $first,
-// 'last' => $last,
-// 'email' => $email
-// );
-
-// $result = mail($to,$first, $message, $data);
-
-
-// header('Content-type:application/json;charset=utf-8');
-// echo json_encode($result);
 
 
 
